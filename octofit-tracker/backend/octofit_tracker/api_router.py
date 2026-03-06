201eee9a738521b7fa115d1ef43c55ba6e659045
Jimmy Majors <jimmym@buildontechnologies.com>
2026-03-06 12:57:10 +0000

    Update Django app for MongoDB, CORS, and API endpoints

---
+++ octofit-tracker/backend/octofit_tracker/api_router.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+from octofit_tracker.views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'teams', TeamViewSet)
+router.register(r'activities', ActivityViewSet)
+router.register(r'leaderboard', LeaderboardViewSet)
+router.register(r'workouts', WorkoutViewSet)
